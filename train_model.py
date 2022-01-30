69458a403fcbc25355ca6ea1639bfdff936fd1a6
Michael Sandbichler <michael.sandbichler@hotmail.com>
2022-01-30 10:35:51 +0100

    Added model training .py file

---
+++ train_model.py
@@ -0,0 +1,39 @@
+# Script to train machine learning model.
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
+# Add the necessary imports for the starter code.
+
+# Add code to load in the data.
+data = pd.read_csv("data/census_nospace.csv")
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
+
+# Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, training=False, label="salary", 
+    categorical_features=cat_features, 
+    encoder = encoder, lb = lb)
+
+# Train and save a model.
+used_model = train_model(X_train, y_train)
+precision, recall, fbeta = compute_model_metrics(y_test,inference(used_model, X_test))
+print("Precision: " + str(precision))
+print("Recall: " + str(recall))
+print("FBeta: " + str(fbeta))
