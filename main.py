612943a9af9ee744ea4034b79038cfca96cce5e1
Michael Sandbichler <michael.sandbichler@hotmail.com>
2022-02-09 22:05:55 +0100

    Added Heroku DVC configuration

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import numpy as np
 import pandas as pd
 from pydantic import BaseModel, Field
 import json
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system(f"dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 from ml.data import process_data
 from ml.model import load_model, inference
